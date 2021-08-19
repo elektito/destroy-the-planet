1922a6e187c375f7d5fb04119f320a460e6c2fc2
Mostafa Razavi <mostafa@sepent.com>
2021-08-19 20:39:16 +0200

    Implement building placement partially

--- World.gd
+++ World.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var placing := -1
+var inside_placing_area := false
 var snap_angles = []
 onready var buttons = [
 	$hud/hbox/toolbox/factory_btn,
@@ -7,6 +9,13 @@ onready var buttons = [
 	$hud/hbox/toolbox/powerplant_btn,
 	$hud/hbox/toolbox/apartment_btn,
 	$hud/hbox/toolbox/bar_btn,
+]
+var preview_icons = [
+	preload("res://assets/gfx/sprites/factory.png"),
+	preload("res://assets/gfx/sprites/mine.png"),
+	preload("res://assets/gfx/sprites/powerplant.png"),
+	preload("res://assets/gfx/sprites/apartment.png"),
+	preload("res://assets/gfx/sprites/bar.png"),
 ]
 
 func _ready():
@@ -17,15 +26,23 @@ func _ready():
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		var v : Vector2 = event.position - $planet.position
-		var angle = get_snap_angle(v.angle())
-		v = Vector2.RIGHT.rotated(angle)
-		$building.position = $planet.position + v.normalized() * 230
-		$building.rotation = v.angle() + PI / 2
+		if placing >= 0:
+			$placing_icon.position = get_local_mouse_position()
+		
+			var v : Vector2 = $placing_area.get_local_mouse_position() - $placing_area/planet.position
+			var angle = get_snap_angle(v.angle())
+			v = Vector2.RIGHT.rotated(angle)
+			$placing_area/preview_icon.position = v.normalized() * 230
+			$placing_area/preview_icon.rotation = v.angle() + PI / 2
+	
 	if event is InputEventMouseButton:
-		var b = $building.duplicate()
-		b.z_index = -10
-		add_child(b)
+		#var b = $building.duplicate()
+		#b.z_index = -10
+		#add_child(b)
+		
+		if placing >= 0 and event.button_index == BUTTON_RIGHT:
+			placing = -1
+			$placing_icon.visible = false
 
 
 func get_snap_angle(angle):
@@ -44,6 +61,11 @@ func get_snap_angle(angle):
 
 func _on_toolbox_btn_pressed(btn):
 	print('pressed ', btn)
+	$placing_icon.texture = buttons[btn].icon
+	$placing_icon.position = get_local_mouse_position()
+	$placing_icon.visible = true
+	placing = btn
+	$placing_area/preview_icon.texture = preview_icons[btn]
 
 
 func _on_toolbox_btn_mouse_entered(btn):
@@ -56,3 +78,17 @@ func _on_toolbox_btn_mouse_entered(btn):
 func _on_toolbox_btn_mouse_exited(btn):
 	print('exit ', btn)
 	$highlight.visible = false
+
+
+func _on_placing_area_mouse_entered():
+	inside_placing_area = true
+	if placing >= 0:
+		$placing_area/preview_icon.visible = true
+		$placing_icon.visible = false
+
+
+func _on_placing_area_mouse_exited():
+	inside_placing_area = false
+	if placing >= 0:
+		$placing_area/preview_icon.visible = false
+		$placing_icon.visible = true
