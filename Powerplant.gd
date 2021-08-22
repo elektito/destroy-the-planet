375f0987bdad89435499bba5e2a4d046cb2a1966
Mostafa Razavi <mostafa@sepent.com>
2021-08-22 03:50:31 +0200

    Update powerplant

--- Powerplant.gd
+++ Powerplant.gd
@@ -1,7 +1,67 @@
 extends Node2D
 
+signal upgraded(building)
+
 const type := Global.BuildingType.POWERPLANT
-const MAX_LEVEL := 8
+
+var levels = [
+	{
+		'number': 1,
+		'description': 'Tiny powerplant.',
+		'base_pollution_per_cycle': 10,
+		'base_resource_usage_per_cycle': 1,
+		'base_power': 1,
+	},
+	{
+		'number': 2,
+		'description': 'Small powerplant.',
+		'base_pollution_per_cycle': 100,
+		'base_resource_usage_per_cycle': 10,
+		'base_power': 10,
+	},
+	{
+		'number': 3,
+		'description': 'Not quite medium powerplant.',
+		'base_pollution_per_cycle': 1000,
+		'base_resource_usage_per_cycle': 100,
+		'base_power': 100,
+	},
+	{
+		'number': 4,
+		'description': 'Medium-sized powerplant.',
+		'base_pollution_per_cycle': 10000,
+		'base_resource_usage_per_cycle': 500,
+		'base_power': 1000,
+	},
+	{
+		'number': 5,
+		'base_pollution_per_cycle': 100000,
+		'base_resource_usage_per_cycle': 1000,
+		'base_power': 10000,
+	},
+	{
+		'number': 6,
+		'description': 'Big powerplant.',
+		'base_pollution_per_cycle': 1000000,
+		'base_resource_usage_per_cycle': 10000,
+		'base_power': 100000,
+	},
+	{
+		'number': 7,
+		'description': 'Huge powerplant.',
+		'base_pollution_per_cycle': 10000000,
+		'base_resource_usage_per_cycle': 50000,
+		'base_power': 1000000,
+	},
+	{
+		'number': 8,
+		'description': 'Gigantic powerplant.',
+		'base_pollution_per_cycle': 100000000,
+		'base_resource_usage_per_cycle': 500000,
+		'base_power': 10000000,
+	},
+]
+var current_level = levels[0]
 
 var building_name = 'Coal Powerplant'
 var description = 'The best kind of power plant there is.'
@@ -16,27 +76,44 @@ func init(world):
 func get_stats():
 	return [
 		{
+			'type': Global.StatType.LEVEL,
+			'value': str(level),
+		},
+		{
 			'type': Global.StatType.POLLUTION,
-			'value': '80',
+			'value': str(get_pollution_per_cycle()),
 		},
 		{
 			'type': Global.StatType.USAGE,
-			'value': '100',
+			'value': str(get_resource_usage_per_cycle()),
 		},
 		{
 			'type': Global.StatType.POWER,
-			'value': '2000',
+			'value': str(get_power_generation()),
 		},
 	]
 
 
+func get_pollution_per_cycle():
+	return current_level['base_pollution_per_cycle']
+
+
+func get_resource_usage_per_cycle():
+	return current_level['base_resource_usage_per_cycle']
+
+
+func get_power_generation():
+	return current_level['base_power']
+
+
 func get_actions():
 	var actions = []
-	if level < MAX_LEVEL:
+	if level < levels[-1]['number']:
 		actions.append({
 			'name': 'level',
 			'title': 'Upgrade to Level ' + str(level + 1),
-			'description': 'Upgrade powerplant to level ' + str(level + 1) + '. Base resource usage will be twice the current amount and the pollution ten times. Electricity production will be ten times.'
+			'description': 'Upgrade powerplant to level ' + str(level + 1) + '. Base resource usage will be twice the current amount and the pollution ten times.',
+			'price': (level + 1) * 1000,
 		})
 	
 	return actions
@@ -46,3 +123,10 @@ func perform_action(action):
 	match action['name']:
 		'level':
 			level += 1
+			current_level = levels[level - 1]
+			emit_signal("upgraded", self)
+
+
+func _on_cycle_timer_timeout():
+	world.produce_pollution(get_pollution_per_cycle())
+	world.consume_resources(get_resource_usage_per_cycle())
