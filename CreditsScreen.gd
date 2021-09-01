7a2b7afc39fabb389f9b52b672c207b64fbe5b29
Mostafa Razavi <mostafa@sepent.com>
2021-09-01 13:15:58 +0200

    Remove old code in credits screen
    We don't need to manually set the building smoke rates
    anymore. Setting the building's "decorative" property automatically
    sets smoke to max.

--- CreditsScreen.gd
+++ CreditsScreen.gd
@@ -4,10 +4,6 @@ onready var credits_y = $credits.rect_position.y
 onready var title_x = $title/game_title.rect_position.x
 
 func _ready():
-	$title/factory/smoke1.rate = 100
-	$title/factory/smoke2.rate = 100
-	$title/powerplant/smoke.rate = 100
-	
 	$credits.rect_position.y = -$credits.rect_size.y - 100
 	$title.position.x = -$title/game_title.rect_size.x - 100
 	
