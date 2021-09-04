aa4d4ade5f3199b63e62a830ebba5d379ae6ce92
Mostafa Razavi <mostafa@sepent.com>
2021-09-04 02:28:12 +0200

    Fix "New Game" button not working in end screen

--- EndScreen.gd
+++ EndScreen.gd
@@ -17,12 +17,11 @@ func _on_exit_btn_pressed():
 func _on_credits_btn_pressed():
 	get_tree().paused = false
 	if get_tree().change_scene("res://CreditsScreen.tscn") != OK:
-		print('change_scene failed. Run for your life!6')
+		print('change_scene failed. Run for your life!')
 
 
 func _on_new_game_btn_pressed():
-	$new_game_dialog.popup_centered()
+	get_tree().paused = false
+	if get_tree().change_scene("res://World.tscn") != OK:
+		print('change_scene failed. Run for your life!')
 
-
-func _on_new_game_dialog_confirmed():
-	get_tree().change_scene("res://World.tscn")
