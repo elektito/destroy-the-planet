957f45b37ad96e1561aebe62f63ac2424bacec33
Mostafa Razavi <mostafa@sepent.com>
2021-09-25 22:26:16 +0200

    Unpause when starting a new game
    We were remaining in paused mode when starting a new game (from the
    settings screen), meaning the game wouldn't work.

--- Settings.gd
+++ Settings.gd
@@ -67,6 +67,7 @@ func _on_new_game_btn_pressed():
 
 func _on_confirm_dialog_confirmed():
 	if action_after_confirm == 'reset':
+		get_tree().paused = false
 		if get_tree().reload_current_scene() != OK:
 			print('Cannot reload current scene. Run for your lives!')
 	elif action_after_confirm == 'exit':
